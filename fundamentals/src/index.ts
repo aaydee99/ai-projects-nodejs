0b83a89522019d6e9e511f2886277b4f7b51161b
Abdullah Akhtar <aaydee99@outlook.com>
2025-02-04 10:13:49 +0500

    Fundamentals Complete

--- fundamentals/src/index.ts
+++ fundamentals/src/index.ts
@@ -14,17 +14,25 @@ const openai = new OpenAI({
 
 const main = async () => {
     //define the prompt 
-    const prompt = "I need to start resistance traning. Can you create a 7 day workout plan for to ease into it? Limit it in 100 words or less.";
+    const prompt = "I need to start resistance training. Can you create a 7 day workout plan for to ease into it? Limit it in 100 words or less.";
 
     //sent api request
     const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [
             {
+                role: "system",
+                content: `You respond with a greeting in the beginning. And you always respond in JSON format, like this: {"greeting" : "greeting here", "plan": "plan here"}`,
+            }
+            ,{
                 role: "user",
                 content: prompt
             }
         ],
+        max_tokens: 200,
+        n: 2,
+        frequency_penalty: 1.5,
+        seed: 5555,
     })
 
     console.log(response.choices[0].message.content);
